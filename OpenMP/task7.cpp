571b968dcbb682240aa837055bcf07dfffd4177c
GatinMI <marat96g@gmail.com>
2017-05-24 04:48:57 +0300

    Update task7.cpp
    Таким образом видно, что распределяется цикл по кускам

--- OpenMP/task7.cpp
+++ OpenMP/task7.cpp
@@ -1,26 +1,20 @@
-//
-// Created by Alexander Leontjev on 10.03.17.
-//
-
 #include <omp.h>
 #include <stdio.h>
 
-int main() {
-    int a[12];
-    int b[12];
-    int c[12];
+int main(){
+	int a[12], b[12], c[12];
 
-#pragma omp parallel num_threads(3)
-    {
+#pragma omp parallel num_threads(12)
+	{
 #pragma omp for schedule(static, 3)
-        for (int i = 0; i < 12; i++) {
-            a[i] = 2;
-            b[i] = 5;
-            printf("thread_num is %d\n", omp_get_thread_num());
-        }
-    }
+		for (int i = 0; i < 12; i++){
+			a[i] = 5;
+			b[i] = 12;
+			printf("number of thread is %d, iteration is %d\n", omp_get_thread_num(), i);
+		}
 
-    for (int i = 0; i < 12; i++) {
+	}
+	for (int i = 0; i < 12; i++) {
         printf("%d ", a[i]);
     }
     printf("\n");
@@ -36,7 +30,7 @@ int main() {
 #pragma omp for schedule(dynamic, 2)
         for (int i = 0; i < 12; i++) {
             c[i] = a[i] + b[i];
-            printf("thread_num is %d\n", omp_get_thread_num());
+            printf("number of thread is %d, iteration is %d\n", omp_get_thread_num(), i);
         }
     }
 
@@ -44,4 +38,5 @@ int main() {
         printf("%d ", c[i]);
     }
     printf("\n");
+	return 0;
 }
